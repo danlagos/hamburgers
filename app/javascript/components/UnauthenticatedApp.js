92ba1a497985659ee4a87f9fcaa4c3b175417a34
hyosunko <hyosunko@gmail.com>
2019-02-06 10:56:12 -0800

    initial commit to devise class

---
+++ app/javascript/components/UnauthenticatedApp.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class UnauthenticatedApp extends React.Component {
+  render () {
+    return (
+      <div>
+        Unauthenticated
+      </div>
+    );
+  }
+}
+
+export default UnauthenticatedApp
